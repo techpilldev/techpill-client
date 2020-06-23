99e242c9954d628fbc83c9cda5d5d1721cedd30d
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-06-23 12:38:33 -0400

    chore: repo commit

--- src/pages/home/home.component.js
+++ src/pages/home/home.component.js
@@ -2,28 +2,28 @@ import React from 'react'
 
 import Title from '../../components/title/title.component.js'
 import Banner from '../../components/banner/banner.component.js'
-import { robotDestop, robotMobile  } from '../../assets/svgs.js'
+import { robotDestop, robotMobile } from '../../assets/svgs.js'
 import { useStyles } from './home.styles.js'
 
 const Home = () => {
   const classes = useStyles()
   const titles = ["THE TECH PILL", "TECHNOLOGICAL EDUCATION"]
   const bannerTitle = (
-    <Title 
-      color='black' 
-      title={titles[0]} 
+    <Title
+      color='black'
+      title={titles[0]}
       subTitle={titles[1]}
     />)
 
   const description = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,`
 
-  return(
+  return (
     <div className={classes.root} >
-      <Banner 
-        title={bannerTitle} 
-        description={description} 
-        mobileImage={robotMobile} 
+      <Banner
+        title={bannerTitle}
+        description={description}
+        mobileImage={robotMobile}
         desktopImage={robotDestop}
       />
       <div>
